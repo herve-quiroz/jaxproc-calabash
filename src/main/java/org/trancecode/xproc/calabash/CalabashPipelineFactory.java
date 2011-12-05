849be9356e2644c29721370140d086bebc55aacb
Herve Quiroz <herve.quiroz@trancecode.org>
2011-12-06 00:04:45 +0100

    Add base classes and configurations files

---
+++ src/main/java/org/trancecode/xproc/calabash/CalabashPipelineFactory.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2011 Herve Quiroz
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.trancecode.xproc.calabash;
+
+import javax.xml.transform.Source;
+
+import org.trancecode.xproc.api.Pipeline;
+import org.trancecode.xproc.api.PipelineFactory;
+
+/**
+ * @author Herve Quiroz
+ */
+public final class CalabashPipelineFactory extends PipelineFactory
+{
+    @Override
+    public Pipeline newPipeline(final Source pipelineSource)
+    {
+        // TODO CalabashPipelineFactory.newPipeline()
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getVersion()
+    {
+        // TODO CalabashPipelineFactory.getVersion()
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getXProcVersion()
+    {
+        // TODO CalabashPipelineFactory.getXProcVersion()
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getXPathVersion()
+    {
+        // TODO CalabashPipelineFactory.getXPathVersion()
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getProductName()
+    {
+        // TODO CalabashPipelineFactory.getProductName()
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getVendor()
+    {
+        // TODO CalabashPipelineFactory.getVendor()
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getVendorUri()
+    {
+        // TODO CalabashPipelineFactory.getVendorUri()
+        throw new UnsupportedOperationException();
+    }
+}
